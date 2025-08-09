22f47e83a874b225cd93ad7cc9efd4881ca7b1c5
medios <161657597+med1@users.noreply.github.com>
2025-08-09 03:48:48 +0300

    Update title.js

--- assets/js/title.js
+++ assets/js/title.js
@@ -1,17 +1,12 @@
 // Array containing different titles to cycle through
 var titles = [
   "@",
-  "@Y",
-  "@Yo",
-  "@Yog",
-  "@Yogh",
-  "@Yoghu",
-  "@Yoghur",
-  "@Yoghurt",
-  "@Yoghurt 1",
-  "@Yoghurt 13",
-  "@Yoghurt 133",
-  "@Yoghurt 1337"
+  "@m",
+  "@me",
+  "@med",
+  "@medi",
+  "@medio",
+  "@medios",
 ];
 
 // Function to change the title periodically
